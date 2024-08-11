b2f45000fbee7bf7535876da17296eaf89d2a388
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2024-08-11 10:58:56 +0100

    updating files

--- JavaScript/j.js
+++
@@ -1,8 +0,0 @@
-let celsius;
-let fahr;
-console.log("Temperature scale conversion table");
-console.log("°C\t°F")
-for (celsius = 20; celsius >= 0; celsius -= 5){
-   fahr = ((celsius * 9)/5 +32);
-   console.log((`${celsius}`)+"\t"+(`${fahr}`));  
-}
