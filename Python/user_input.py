eb9b6167920036065956730960da2fe89628c86d
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2024-09-17 20:01:55 +0100

    updating files

--- Python/user_input.py
+++ Python/user_input.py
@@ -1,6 +1,6 @@
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 location = input("Enter your location: ")
-message = f'Hello, {name}. You are {age} years old. You live at {location}.'
+message = f'Hello, {name} you are {age} years old and you live in {location}.'
 print(message)
 
