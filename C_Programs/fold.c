74762d4c9c144e266986642feb14d5884aa05761
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2024-09-17 19:52:00 +0100

    Updating and adding new files

---
+++ C_Programs/fold.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char array[5];
+int i = 0;
+
+int main() {
+    int c = getchar();
+    while (c != EOF && i < 5) {
+        array[i] = c;
+        i++;
+        c = getchar();
+    }
+    for (int j = 0; j < i; j++) {
+        printf("%c", array[j]);
+    }
+    return 0;
+}
+
---
+++ Python/user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+message = f'Hello, {name}. You are {age} years old. You live at {location}.'
+print(message)
+
