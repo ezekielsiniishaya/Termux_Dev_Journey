b1e3aa2093b1bec6ecd87d88836e33f4b5111d43
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2024-10-28 21:13:57 +0100

    Update week3_Ass.py

--- Python/week3_Ass.py
+++ Python/week3_Ass.py
@@ -1,12 +1,12 @@
+#Function to calculate discount to a price and print new price
 def calculate_discount(price, discount_percent):
     if (discount_percent >= 20):
         new_price = price - price * (discount_percent/100)
         print("Discounted Price: ", new_price)
     else:
         print("Prince: ", price)
-
+#Receiving the price and discount from user, converting to float since by default, input converts to string
 price = float(input("Enter the Prince: "))
-#print(type(price))
 discount_percent = float(input("Enter the discount: "))
-#print(type(discount_percent))
+#calling the function
 calculate_discount(price, discount_percent)
