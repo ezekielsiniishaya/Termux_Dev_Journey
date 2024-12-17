b8c9e4e023a2d326e0ac66202c57ec2b6ebd86da
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2024-12-17 19:01:54 +0100

    adding insertion sort javascript program and renaming sort.js to selection_sort.js :

---
+++ JavaScript/insertion_sort.js
@@ -0,0 +1,22 @@
+// This is a program that uses insertion sort to sort an array
+
+// This fuction inserts a value in a particular position in an array when some conditions are met
+var insert = function (array, rightIndex, value) {
+  var i;
+
+  for (i = rightIndex; i >= 0 && array[i] > value; i--) {
+    array[i + 1] = array[i];
+  }
+  array[i + 1] = value;
+};
+// This function loops through the array inserting the current value in the right position in the sorted subarray
+var insertionSort = function (array) {
+  for (var i = 1; i < array.length; i++) {
+    insert(array, i - 1, array[i]);
+  }
+};
+
+var array = [3, 5, 7, 11, 13, 2, 9, 6];
+console.log(JSON.stringify(array));
+insertionSort(array);
+console.log(JSON.stringify(array));
