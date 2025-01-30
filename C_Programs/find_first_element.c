4b6c8e606a1611d04c2934c22a1b636c2f8a93a6
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2025-01-30 15:42:43 +0100

    adding C programs

--- C_Programs/find_first_element.c
+++ C_Programs/find_first_element.c
@@ -2,52 +2,45 @@
 #define MAX_ROWS 1000
 #define MAX_COLUMNS 1000
 
-int find_first_element(int array[][MAX_COLUMNS], int row)
-{
-    for (int i = 0; i < row; i++)
-    {
-        int column = 0;
-
-        if (array[i][column] != ' ' && array[row][column] != '\t')
-        {
-            return array[i][column];
-        }
-        return -1;
+int find_first_element(int array[][MAX_COLUMNS], int row) {
+  int column = 0;
+  for (int i = 0; i < row; i++) {
+    if (array[i][column] != ' ' && array[row][column] != '\t') {
+      return array[i][column];
+      break;
     }
+    column++;
+  }
+  return -1;
 }
-int main()
-{
-    int array_2D[MAX_ROWS][MAX_COLUMNS]; // 2D array to store code input
-    int row, column;
-    int exit_status = 0;
-    // Prompt the user to enter lines of code
-    printf(
-        "Enter %d lines of code (each line up to %d characters). All "
-        "comments will be cleared:\n",
-        MAX_ROWS, MAX_COLUMNS - 1);
-    for (row = 0; row < MAX_ROWS; row++)
-    {
-        column = 0;
-        while (column < MAX_COLUMNS - 1)
-        {
-            int character = getchar();
-            if (character == EOF)
-            {
-                exit_status = 1;
-                break;
-            }
-            if (character == '\n')
-            {
-                break;
-            }
-            array_2D[row][column++] = character;
-        }
-        array_2D[row][column] = '\0'; // Null-terminate the string
-        if (exit_status == 1)
-        {
-            break;
-        }
+int main() {
+  int array_2D[MAX_ROWS][MAX_COLUMNS];  // 2D array to store code input
+  int row, column;
+  int exit_status = 0;
+  // Prompt the user to enter lines of code
+  printf(
+      "Enter %d lines of code (each line up to %d characters). All "
+      "comments will be cleared:\n",
+      MAX_ROWS, MAX_COLUMNS - 1);
+  for (row = 0; row < MAX_ROWS; row++) {
+    column = 0;
+    while (column < MAX_COLUMNS - 1) {
+      int character = getchar();
+      if (character == EOF) {
+        exit_status = 1;
+        break;
+      }
+      if (character == '\n') {
+        break;
+      }
+      array_2D[row][column++] = character;
     }
-    int element = find_first_element(array_2D, 0);
-    putchar(element);
+    array_2D[row][column] = '\0';  // Null-terminate the string
+    if (exit_status == 1) {
+      break;
+    }
+  }
+  int element = find_first_element(array_2D, 1);
+  putchar(element);
+  putchar('\n');
 }
