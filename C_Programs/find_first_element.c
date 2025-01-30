e0d100454c38b9f9c21c527969fc9e79ff1d5ce0
Ezekiel Ishaya <your_ezekielsiniishaya@gmail.com>
2025-01-30 14:59:52 +0100

    adding c programs from pc

--- C_Programs/find_first_element.c
+++ C_Programs/find_first_element.c
@@ -1,16 +1,53 @@
-for (int row = 0; row < MAX_ROWS; row++) {
-        int column = 0;
-        int is_empty = 1;  // Flag to check if the line is empty
+#include <stdio.h>
+#define MAX_ROWS 1000
+#define MAX_COLUMNS 1000
 
-        // Check if the current line has any non-null characters
-        while (array[row][column] != '\0') {
-            if (array[row][column] != ' ' && array[row][column] != '\t') {
-                is_empty = 0;  // Line is not empty if there's a non-space character
+int find_first_element(int array[][MAX_COLUMNS], int row)
+{
+    for (int i = 0; i < row; i++)
+    {
+        int column = 0;
+
+        if (array[i][column] != ' ' && array[row][column] != '\t')
+        {
+            return array[i][column];
+        }
+        return -1;
+    }
+}
+int main()
+{
+    int array_2D[MAX_ROWS][MAX_COLUMNS]; // 2D array to store code input
+    int row, column;
+    int exit_status = 0;
+    // Prompt the user to enter lines of code
+    printf(
+        "Enter %d lines of code (each line up to %d characters). All "
+        "comments will be cleared:\n",
+        MAX_ROWS, MAX_COLUMNS - 1);
+    for (row = 0; row < MAX_ROWS; row++)
+    {
+        column = 0;
+        while (column < MAX_COLUMNS - 1)
+        {
+            int character = getchar();
+            if (character == EOF)
+            {
+                exit_status = 1;
                 break;
             }
-            column++;
+            if (character == '\n')
+            {
+                break;
+            }
+            array_2D[row][column++] = character;
         }
-
-        // Only print the line if it's not empty
-        if (!is_empty) {
-    
+        array_2D[row][column] = '\0'; // Null-terminate the string
+        if (exit_status == 1)
+        {
+            break;
+        }
+    }
+    int element = find_first_element(array_2D, 0);
+    putchar(element);
+}
