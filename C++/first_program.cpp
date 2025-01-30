2fa549e75988b48fa8e26adcaa3e000e35cb132b
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2025-01-30 14:32:40 +0100

    adding C programs

---
+++ C++/first_program.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int a = 1;
+  int b = 2;
+  int c = a + b;
+  std::cout << "Hello, World!" << std::endl;
+  std::cout << c << " " << std::endl;
+  return 0;
+}
