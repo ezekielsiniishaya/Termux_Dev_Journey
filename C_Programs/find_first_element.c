c2ea1d9c598667a2008a1ef4371dfedd527e542b
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2025-01-31 04:09:59 +0100

    adding C programs

--- C_Programs/find_first_element.c
+++ C_Programs/find_first_element.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
+
 #define MAX_ROWS 1000
 #define MAX_COLUMNS 1000
 
-int find_first_element(int array[][MAX_COLUMNS], int row) {
+// Function to find the first non-whitespace character in a row
+int find_first_element(char array[][MAX_COLUMNS], int row) {
   int column = 0;
-  for (int i = 0; i < row; i++) {
-    if (array[i][column] != ' ' && array[row][column] != '\t') {
-      return array[i][column];
-      break;
+  while (array[row][column] != '\0') {
+    if (array[row][column] != ' ' && array[row][column] != '\t') {
+      return array[row][column];  // Return first non-whitespace character
     }
     column++;
   }
-  return -1;
+  return -1;  // No valid character found
 }
+
 int main() {
-  int array_2D[MAX_ROWS][MAX_COLUMNS];  // 2D array to store code input
+  char array_2D[MAX_ROWS][MAX_COLUMNS];  // Use char instead of int for text storage
   int row, column;
   int exit_status = 0;
-  // Prompt the user to enter lines of code
+
   printf(
-      "Enter %d lines of code (each line up to %d characters). All "
-      "comments will be cleared:\n",
+      "Enter up to %d lines of code (each line up to %d characters). Press "
+      "Ctrl+D to stop:\n",
       MAX_ROWS, MAX_COLUMNS - 1);
+
   for (row = 0; row < MAX_ROWS; row++) {
     column = 0;
     while (column < MAX_COLUMNS - 1) {
@@ -33,14 +36,12 @@ int main() {
       if (character == '\n') {
         break;
       }
-      array_2D[row][column++] = character;
+      array_2D[row][column++] = (char)character;
     }
     array_2D[row][column] = '\0';  // Null-terminate the string
-    if (exit_status == 1) {
+    if (exit_status) {
       break;
     }
   }
-  int element = find_first_element(array_2D, 1);
-  putchar(element);
-  putchar('\n');
+return 0;
 }
