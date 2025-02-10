20eacf9191e64355cdfdec5e459ef28d7a961507
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2025-02-10 08:02:41 +0100

    input.cpp

--- C++/input.cpp
+++ C++/input.cpp
@@ -2,16 +2,17 @@
 #include <string>
 
 int main() {
-    std::string name;
-    int age;
+  std::string name;
+  int age;
 
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-    
-    std::cout << "Enter your age: ";
-    std::cin >> age;
-    
-    std::cout << "Nice to meet you, " << name << "! You are " << age << " years old." << std::endl;
-    
-    return 0;
+  std::cout << "Enter your name: ";
+  std::getline(std::cin, name);
+
+  std::cout << "Enter your age: ";
+  std::cin >> age;
+
+  std::cout << "Nice to meet you, " << name << "! You are " << age
+            << " years old." << std::endl;
+
+  return 0;
 }
